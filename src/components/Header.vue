a4ed35421fadfa0f398dd2e63c586c0e186b2089
sqnguyen820 <sqnguyen829@gmail.com>
2021-05-22 18:16:06 -0500

    updated to vuex

---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+    <header>
+        <h1 class="text-center">Flatiron Stock Exchange</h1>
+      </header>
+</template>
---
+++ src/containers/MainContainer.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <SearchBar/>
+
+          <div class="row">
+            <div class="col-8">
+
+              <StockContainer/>
+
+            </div>
+            <div class="col-4">
+
+              <PortfolioContainer/>
+
+            </div>
+          </div>
+      </div>
+</template>
+
+<script>
+import SearchBar from '../components/SearchBar'
+import StockContainer from './StockContainer'
+import PortfolioContainer from './PortfolioContanier'
+export default {
+    name:"MainContainer",
+    components:{
+        SearchBar,
+        StockContainer,
+        PortfolioContainer
+    }
+}
+</script>
